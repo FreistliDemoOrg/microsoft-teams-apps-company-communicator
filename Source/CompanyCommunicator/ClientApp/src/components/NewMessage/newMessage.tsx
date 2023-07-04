1e5db152a20f344ec02bb570fa3dac24413bffdb
Freist Li <fauna179@hotmail.com>
2023-07-04 13:12:22 +0800

    Update newMessage.tsx

--- Source/CompanyCommunicator/ClientApp/src/components/NewMessage/newMessage.tsx
+++ Source/CompanyCommunicator/ClientApp/src/components/NewMessage/newMessage.tsx
@@ -834,15 +834,7 @@ export const NewMessage = () => {
                       ))}
                     </Combobox>
                   </div>
-                )}
-                <Radio id='radio3' value={AudienceSelection.AllUsers} label={t('SendToAllUsers')} />
-                <div className={cmb_styles.root}>
-                  {selectedRadioButton === AudienceSelection.AllUsers && (
-                    <Text id='radio3Note' role={allUsersAria} className='info-text'>
-                      {t('SendToAllUsersNote')}
-                    </Text>
-                  )}
-                </div>
+                )}                
                 <Radio id='radio4' value={AudienceSelection.Groups} label={t('SendToGroups')} />
                 {selectedRadioButton === AudienceSelection.Groups && (
                   <div className={cmb_styles.root}>
